01b85a5bf65e5bb03ea326f8008e8629ce27428e
TekzinDev <aryannesousa04@gmail.com>
2026-06-09 20:45:52 -0300

    Remove build scripts

--- build_deepscan.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0DeepScan"
-"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" DeepScan.vcxproj /p:Configuration=Release /p:Platform=x64 /v:m
